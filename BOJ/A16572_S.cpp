032e136a09897130a7548e797baec5b3b15900c0
sbrus1213 <rlgus1590@gmail.com>
2019-11-20 17:20:47 +0900

    2018 Sogang Programming Contest - Champion
    16567 바이너리 왕국 - 구현
    16568 엔비스카의 영혼 - Dynamic Programming
    16569 화산쇄설류 - BFS
    16570 앞뒤가 맞는 수열 - KMP - fail function
    16571 알파 틱택토 - Dynamic Programming? Back tracking?
    16572 Pixel Triangles - Dynamic Programming???

---
+++ BOJ/A16572_S.cpp
@@ -0,0 +1,32 @@
+// BOJ 16572 Pixel Triangles
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, pixel[2001][2001], ans;
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cin >> n;
+
+	for (int i = 0; i < n; ++i) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		pixel[a][b] = max(pixel[a][b], c);
+	}
+
+	for (int i = 1; i <= 2000; ++i) {
+		for (int j = 1; j <= 2000; ++j) {
+			if (pixel[i][j] > 1) {
+				if (i + 1 <= 2000) pixel[i + 1][j] = max(pixel[i + 1][j], pixel[i][j] - 1);
+				if (j + 1 <= 2000) pixel[i][j + 1] = max(pixel[i][j + 1], pixel[i][j] - 1);
+			}
+			if (pixel[i][j]) ans++;
+		}
+	}
+
+	cout << ans;
+}
